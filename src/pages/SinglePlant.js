70dfc7c0da03b15b9979d8525a36d0cc11f6b358
Anthea Ip <antheaip964@gmail.com>
2021-12-29 20:28:51 -0500

    With SinglePlant

--- src/pages/SinglePlant.js
+++ src/pages/SinglePlant.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const SinglePlant = (props) => {
-  return <h1>Post</h1>;
+// Destructuring the props needed to get our plant, including router prop match
+
+
+const SinglePlant = ({ plants, match }) => {
+  const id = parseInt(match.params.id);
+  const plant = plants.find((plant) => plant.id === id);
+
+  // Styles
+
+  const div = {
+    textAlign: "center",
+    border: "3px solid green",
+    width: "80%",
+    margin: "30px auto",
+  };
+
+
+
+
+  return (
+  <div style={div}>
+    <h1>{plant.subject}</h1>
+    <h2>{plant.details}</h2>
+    <Link to="/">
+      <button>Go Back</button>
+    </Link>
+  </div>
+);
 };
 
 export default SinglePlant;
